d1c8ac0ea507a43e704f53318196fd28d4635ef3
yasminanr <ysmnneera@gmail.com>
2023-01-07 13:03:32 +0700

    Add query for question 3

--- CASE 4/SQL Query/cust_transactions.sql
+++ CASE 4/SQL Query/cust_transactions.sql
@@ -30,4 +30,24 @@ FROM transactions
 WHERE txn_type = 'deposit';
 
 -- Question 3: For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+WITH transactions_aggregate AS (
+	SELECT
+		MONTH(txn_date) AS txn_month,
+		customer_id,
+		SUM(CASE WHEN txn_type = 'deposit' THEN 1 ELSE 0 END) AS deposit_count,
+		SUM(CASE WHEN txn_type = 'purchase' THEN 1 ELSE 0 END) AS purchase_count,
+		SUM(CASE WHEN txn_type = 'withdrawal' THEN 1 ELSE 0 END) AS withdrawal_count
+	FROM data_bank.customer_transactions
+	GROUP BY
+		MONTH(txn_date),
+		customer_id
+)
+
+SELECT 
+	txn_month,
+	COUNT(customer_id) AS customer_count
+FROM transactions_aggregate
+WHERE deposit_count > 1 AND (purchase_count = 1 OR withdrawal_count = 1)
+GROUP BY txn_month
+ORDER BY txn_month;
     
